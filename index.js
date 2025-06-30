3f802c93e2e2776c04508ccb418d7b1682d6c390
Aleks Hudochenkov <aleks@hudochenkov.com>
2025-07-01 01:03:44 +0200

    Use new categories for built-in rules

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import { bestPractices } from './built-in/bestPractices.js';
-import { es6 } from './built-in/es6.js';
+import { possibleProblems } from './built-in/possibleProblems.js';
+import { suggestions } from './built-in/suggestions.js';
 import { importRules } from './plugin/import.js';
 import { jest } from './plugin/jest.js';
 import { jestDom } from './plugin/jest-dom.js';
 import { jsxA11y } from './plugin/jsx-a11y.js';
-import { possibleErrors } from './built-in/possibleErrors.js';
 import { react as reactPlugin } from './plugin/react.js';
-import { stylisticIssues } from './built-in/stylisticIssues.js';
 import { stylistic } from './plugin/stylistic.js';
 import { testingLibrary } from './plugin/testing-library.js';
 import { unicorn } from './plugin/unicorn.js';
-import { variables } from './built-in/variables.js';
 
-const base = defineConfig([possibleErrors, bestPractices, variables, stylisticIssues, es6]);
+const base = defineConfig([possibleProblems, suggestions]);
 
 const commonPlugins = defineConfig([stylistic, unicorn, jest, importRules]);
 
